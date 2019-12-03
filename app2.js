856c2bca95850f64c113351a9399f2a79633bc0c
Christopher Khalife <khalife.chris@gmail.com>
2019-12-02 20:06:45 -0800

    Cover Month Tabs

---
+++ app2.js
@@ -0,0 +1,24 @@
+var express         = require("express"),
+    app             = express(),
+    http            = require('http'),
+    hostname        = '127.0.0.1',
+    port            = 3000,
+    bassTabRoute    = require("./routes/bassTabs");
+
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + "/public"));
+
+app.get("/", function(req, res) {
+    res.render("Home/home");
+});
+
+app.use("/bass-tabs", bassTabRoute);
+
+//Gear Page
+app.get("/gear", function(req,res) {
+    res.render("gear/gear");
+});
+
+app.listen(port, hostname, function(){
+  console.log("Bass tabs started");
+});
