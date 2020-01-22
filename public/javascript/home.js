a09b59a8e9c08425bc1e7090673c236c1f10e55a
Christopher Khalife <khalife.chris@gmail.com>
2020-01-21 19:08:01 -0800

    Design 2.0

---
+++ public/javascript/home.js
@@ -0,0 +1,46 @@
+var myNav = $(".topnav");
+window.onscroll = function () { 
+    if (window.pageYOffset >= 200 ) {
+        myNav.classList.add("navTransparent");
+    } 
+    else {
+        myNav.classList.remove("navTransparent");
+    }
+};
+
+
+//Fucntion returns how far picture animation should move to the right
+function animateLength() {
+    var width = window.innerWidth;
+    var animation_width;
+    if (width < 600) {
+        animation_width = (.12*$('#aboutimg').parent().width())+'px';
+        return [10, 320, animation_width];
+    }
+    else if (width < 720) {
+        animation_width = (.12*$('#aboutimg').parent().width())+'px';
+        return [10, 500, animation_width];
+    }
+    else if (width < 980) {
+        animation_width = (.23*$('#aboutimg').parent().width())+'px';
+        return [30, 740, animation_width];
+    }
+    else {
+        animation_width = (.20*$('#aboutimg').parent().width())+'px';
+        return [160, 780, animation_width];
+    }
+}
+
+var animate = false;
+window.onscroll = function () {
+    widthArray = animateLength();
+    console.log(window.innerWidth);
+    if (window.pageYOffset >= widthArray[0] && window.pageYOffset <= widthArray[1] && !animate) {
+        $("#aboutimg").animate({left: "+=" + widthArray[2]}, 450);
+        animate = true;
+    }
+    else if ((window.pageYOffset < widthArray[0] || window.pageYOffset > widthArray[1]) && animate) {
+        $("#aboutimg").animate({left: "-=" + widthArray[2]}, 450);
+        animate = false;
+    }
+};
