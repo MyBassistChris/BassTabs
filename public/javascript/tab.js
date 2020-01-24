20456edc6c9d78420285b2016a04d7f2620f9913
Christopher Khalife <khalife.chris@gmail.com>
2020-01-23 20:52:17 -0800

    No video change

--- public/javascript/tab.js
+++ public/javascript/tab.js
@@ -2,7 +2,6 @@ var videoUrl;
 
 $(document).ready(function() {
     addVideoAndTuning();
-    videoWidth();
 });
 
 $(window).resize(function() {
@@ -13,6 +12,7 @@ function addVideoAndTuning() {
     if($(".videoUrl").length > 0) { //If tab is associated with video
         videoUrl = document.querySelector(".videoUrl").innerHTML;
         document.querySelector(".videoUrl").remove();
+        videoWidth();
     }
     if($("#tuning").length > 0) { //If tuning is not standard then add custom tuning
         document.querySelector("#tuning").innerHTML = document.querySelector(".tuning").innerHTML;
@@ -21,13 +21,15 @@ function addVideoAndTuning() {
 }
 
 function videoWidth() {
-    var containerWidth = $(".tab-container").width();
-    var tabWidth = $(".tab").width();
-    var widthDiff = containerWidth - tabWidth;
-    if(widthDiff < 340) {
-        document.querySelector("#video").innerHTML = "<iframe width=\"425\" height=\"260\" src=\"https://www.youtube.com/embed/" + videoUrl + "\" frameborder=\"0\" allow=\"accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen></iframe>"
-    }
-    else {
-        document.querySelector("#video").innerHTML = "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/" + videoUrl + "\" frameborder=\"0\" allow=\"accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen></iframe>"
+    if(document.querySelector("#video").length > 0) {
+        var containerWidth = $(".tab-container").width();
+        var tabWidth = $(".tab").width();
+        var widthDiff = containerWidth - tabWidth;
+        if(widthDiff < 340) {
+            document.querySelector("#video").innerHTML = "<iframe width=\"425\" height=\"260\" src=\"https://www.youtube.com/embed/" + videoUrl + "\" frameborder=\"0\" allow=\"accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen></iframe>"
+        }
+        else {
+            document.querySelector("#video").innerHTML = "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/" + videoUrl + "\" frameborder=\"0\" allow=\"accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen></iframe>"
+        }
     }
 }
